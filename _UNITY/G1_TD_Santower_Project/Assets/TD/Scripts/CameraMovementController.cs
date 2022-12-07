f54d1af0b22267f88516c78db976207ac469f52a
Tykoph <45920212+Tykoph@users.noreply.github.com>
2022-12-07 16:25:25 +0100

    RETAKE Level_One ans try ADD camera placement on minimap (not effective)

--- _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/CameraMovementController.cs
+++ _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/CameraMovementController.cs
@@ -7,25 +7,24 @@ using UnityEngine.Rendering.UI;
 public class CameraMovementController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _movementLimits;
-
-    [SerializeField]
     private float _cameraSpeed = 25f;
 
     [SerializeField]
-    private Vector3 _boxColliderSizeLevel1;   
+    private List<GameObject> _movementLimits = new List<GameObject>();
+
     [SerializeField]
-    private Vector3 _boxColliderSizeLevel2;    
+    private List<float> _boxColliderSize = new List<float>();   
+    
     [SerializeField]
-    private Vector3 _boxColliderSizeLevel3;    
-    [SerializeField]
-    private Vector3 _boxColliderSizeLevel4;
+    private List<float> _CameraYAxis = new List<float>();
 
     private CinemachineConfiner _confiner;
 
     private BoxCollider _boxCollider;
 
 	private GameManager _gameManager;
+
+    private GameObject _movementLimit;
 
 
 	private void Awake()
@@ -40,7 +39,8 @@ public class CameraMovementController : MonoBehaviour
 	{
         _gameManager.GamePhaseChangeEvent_UE.RemoveListener(UpdateConfiner);
         _gameManager.GamePhaseChangeEvent_UE.AddListener(UpdateConfiner);
-        _boxCollider.transform.localScale = _boxColliderSizeLevel1;
+        _boxCollider.transform.localScale = new Vector3(_boxColliderSize[0], 1, _boxColliderSize[0]);
+        _movementLimit = _movementLimits[0];
 	}
 
 	private void OnDisable()
@@ -52,12 +52,12 @@ public class CameraMovementController : MonoBehaviour
     { 
 		transform.position = new Vector3
             (Mathf.Clamp(transform.position.x + _cameraSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
-                -_movementLimits.transform.position.x, _movementLimits.transform.position.x), 
+                -_movementLimit.transform.position.x, _movementLimit.transform.position.x), 
             
             transform.position.y,
 			
             Mathf.Clamp(transform.position.z + _cameraSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 
-                -_movementLimits.transform.position.z, _movementLimits.transform.position.z));
+                -_movementLimit.transform.position.z, _movementLimit.transform.position.z));
 
 	}
 
@@ -65,19 +65,27 @@ public class CameraMovementController : MonoBehaviour
     {
         if (toPhase == GameManager.GamePhase.Phase1)
         {
-            _boxCollider.transform.localScale = _boxColliderSizeLevel1;
+			_boxCollider.transform.localScale = new Vector3(_boxColliderSize[0], 1, _boxColliderSize[0]);            
+            transform.position = new Vector3 (transform.position.x, _CameraYAxis[0], transform.position.z);
+            _movementLimit = _movementLimits[0];
         }
         if (toPhase == GameManager.GamePhase.Phase2)
         {
-            _boxCollider.transform.localScale = _boxColliderSizeLevel2;
+			_boxCollider.transform.localScale = new Vector3(_boxColliderSize[1], 1, _boxColliderSize[1]);
+			transform.position = new Vector3 (transform.position.x, _CameraYAxis[1], transform.position.z);
+            _movementLimit = _movementLimits[1];
         }
         if (toPhase == GameManager.GamePhase.Phase3)
         {
-            _boxCollider.transform.localScale = _boxColliderSizeLevel3;
+            _boxCollider.transform.localScale = new Vector3(_boxColliderSize[2], 1, _boxColliderSize[2]);
+            transform.position = new Vector3 (transform.position.x, _CameraYAxis[2], transform.position.z);
+            _movementLimit = _movementLimits[2];
         }
         if (toPhase == GameManager.GamePhase.Phase4)
         {
-            _boxCollider.transform.localScale = _boxColliderSizeLevel4;
+            _boxCollider.transform.localScale = new Vector3(_boxColliderSize[3], 1, _boxColliderSize[3]);
+            transform.position = new Vector3 (transform.position.x, _CameraYAxis[3], transform.position.z);
+            _movementLimit = _movementLimits[3];
         }
     }
 }
