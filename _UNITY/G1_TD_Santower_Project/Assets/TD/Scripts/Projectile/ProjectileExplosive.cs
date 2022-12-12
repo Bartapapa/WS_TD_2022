f520a3d63ce233baa3fdfe5500c753d321843f9f
Barthelemi Rapeaux <rapeauxb@gmail.com>
2022-12-12 21:01:29 +0100

    ADD: AnimationCurve for artillery, implemented correct timing of explosion for exploding enemies.

--- _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/Projectile/ProjectileExplosive.cs
+++ _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/Projectile/ProjectileExplosive.cs
@@ -33,6 +33,10 @@
         private bool _canProduceShrapnel = false;
         private bool _hasExploded = false;
 
+        public void SetDestination(Vector3 setDestination)
+        {
+            _destination = setDestination;
+        }
         public bool CanProduceShrapnel => _canProduceShrapnel;
 
         private void Awake()
@@ -108,45 +112,72 @@
         IEnumerator ArcProjectile()
         {
             _isMoving = true;
-            RaycastHit hit;
-            Vector3 destinationPoint = Vector3.zero;
-            if (Physics.Raycast(transform.position, transform.forward, out hit, float.MaxValue, _enemyLayer))
+            float time = 0f;
+            Vector3 startPosition = transform.position;
+
+            _destination = Target.transform.position - Target.transform.forward;
+
+            Debug.Log(startPosition);
+            Debug.Log(_destination);
+
+            float duration = 60f / _projectileSpeed;
+
+            while (time < duration || !_hitThing)
             {
-                destinationPoint = hit.point;
+                time += Time.deltaTime;
+
+                float linearT = time / duration;
+                float heightT = _curve.Evaluate(linearT);
+
+                float height = Mathf.Lerp(0f, _projectileMaxHeight, heightT);
+
+                transform.position = Vector3.Lerp(startPosition, _destination, linearT) + new Vector3(0f, height);
+
+                yield return null;
             }
+
+            EXPLOSION();
+
+            //_isMoving = true;
+            //RaycastHit hit;
+            //Vector3 destinationPoint = Vector3.zero;
+            //if (Physics.Raycast(transform.position, transform.forward, out hit, float.MaxValue, _enemyLayer))
+            //{
+            //    destinationPoint = hit.point;
+            //}
             
-            float travelDistance = Vector3.Distance(transform.position, destinationPoint);
-            float halfTravelDistance = travelDistance * .5f;
-            float distanceTravelled = 0f;
-            Vector3 currentPos = transform.position;
-            float distanceRatio = 0f;
+            //float travelDistance = Vector3.Distance(transform.position, destinationPoint);
+            //float halfTravelDistance = travelDistance * .5f;
+            //float distanceTravelled = 0f;
+            //Vector3 currentPos = transform.position;
+            //float distanceRatio = 0f;
 
-            while (distanceTravelled < halfTravelDistance)
-            {
-                transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
-                distanceTravelled += Vector3.Distance(currentPos, transform.position);
-                currentPos = transform.position;
+            //while (distanceTravelled < halfTravelDistance)
+            //{
+            //    transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
+            //    distanceTravelled += Vector3.Distance(currentPos, transform.position);
+            //    currentPos = transform.position;
 
-                distanceRatio = 1 - (distanceTravelled / halfTravelDistance);
-                float rise = _projectileSpeed * distanceRatio * Time.deltaTime;
-                transform.position += new Vector3(0, rise, 0);
+            //    distanceRatio = 1 - (distanceTravelled / halfTravelDistance);
+            //    float rise = _projectileSpeed * distanceRatio * Time.deltaTime;
+            //    transform.position += new Vector3(0, rise, 0);
 
-                yield return null;
-            }
-            distanceTravelled = 0f;
-            while (distanceTravelled < halfTravelDistance || !_hitThing)
-            {
-                transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
-                distanceTravelled += Vector3.Distance(currentPos, transform.position);
-                currentPos = transform.position;
+            //    yield return null;
+            //}
+            //distanceTravelled = 0f;
+            //while (distanceTravelled < halfTravelDistance || !_hitThing)
+            //{
+            //    transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
+            //    distanceTravelled += Vector3.Distance(currentPos, transform.position);
+            //    currentPos = transform.position;
 
-                distanceRatio = distanceTravelled / halfTravelDistance;
-                float fall = _projectileSpeed * distanceRatio * Time.deltaTime;
-                transform.position += new Vector3(0, -fall, 0);
+            //    distanceRatio = distanceTravelled / halfTravelDistance;
+            //    float fall = _projectileSpeed * distanceRatio * Time.deltaTime;
+            //    transform.position += new Vector3(0, -fall, 0);
 
-                yield return null;
-            }
-            yield return null;
+            //    yield return null;
+            //}
+            //yield return null;
         }
 
         public void SetCanProduceShrapnel(bool value)
--- _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/Projectile/ProjectileNapalm.cs
+++ _UNITY/G1_TD_Santower_Project/Assets/TD/Scripts/Projectile/ProjectileNapalm.cs
@@ -22,6 +22,7 @@ public class ProjectileNapalm : AProjectile
 	private bool _hasExploded = false;
 
 
+
 	private void Update()
 	{
 		MoveForward();
@@ -69,46 +70,70 @@ public class ProjectileNapalm : AProjectile
 
 	IEnumerator ArcProjectile()
 	{
-		_isMoving = true;
-		RaycastHit hit;
-		Vector3 destinationPoint = Vector3.zero;
-		if (Physics.Raycast(transform.position, transform.forward, out hit, float.MaxValue, _enemyLayer))
-		{
-			destinationPoint = hit.point;
-		}
+        _isMoving = true;
+        float time = 0f;
+        Vector3 startPosition = transform.position;
 
-		float travelDistance = Vector3.Distance(transform.position, destinationPoint);
-		float halfTravelDistance = travelDistance * .5f;
-		float distanceTravelled = 0f;
-		Vector3 currentPos = transform.position;
-		float distanceRatio = 0f;
+        _destination = Target.transform.position;
 
-		while (distanceTravelled < halfTravelDistance)
-		{
-			transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
-			distanceTravelled += Vector3.Distance(currentPos, transform.position);
-			currentPos = transform.position;
+        float duration = 60f / _projectileSpeed;
 
-			distanceRatio = 1 - (distanceTravelled / halfTravelDistance);
-			float rise = _projectileSpeed * distanceRatio * Time.deltaTime;
-			transform.position += new Vector3(0, rise, 0);
+        while (time < duration || !_hitThing)
+        {
+            time += Time.deltaTime;
 
-			yield return null;
-		}
-		distanceTravelled = 0f;
-		while (distanceTravelled < halfTravelDistance || !_hitThing)
-		{
-			transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
-			distanceTravelled += Vector3.Distance(currentPos, transform.position);
-			currentPos = transform.position;
+            float linearT = time / duration;
+            float heightT = _curve.Evaluate(linearT);
 
-			distanceRatio = distanceTravelled / halfTravelDistance;
-			float fall = _projectileSpeed * distanceRatio * Time.deltaTime;
-			transform.position += new Vector3(0, -fall, 0);
+            float height = Mathf.Lerp(0f, _projectileMaxHeight, heightT);
 
-			yield return null;
-		}
-		yield return null;
+            transform.position = Vector3.Lerp(startPosition, _destination, linearT) + new Vector3(0f, height);
+
+            yield return null;
+        }
+
+		EXPLOSION();
+
+  //      _isMoving = true;
+		//RaycastHit hit;
+		//Vector3 destinationPoint = Vector3.zero;
+		//if (Physics.Raycast(transform.position, transform.forward, out hit, float.MaxValue, _enemyLayer))
+		//{
+		//	destinationPoint = hit.point;
+		//}
+
+		//float travelDistance = Vector3.Distance(transform.position, destinationPoint);
+		//float halfTravelDistance = travelDistance * .5f;
+		//float distanceTravelled = 0f;
+		//Vector3 currentPos = transform.position;
+		//float distanceRatio = 0f;
+
+		//while (distanceTravelled < halfTravelDistance)
+		//{
+		//	transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
+		//	distanceTravelled += Vector3.Distance(currentPos, transform.position);
+		//	currentPos = transform.position;
+
+		//	distanceRatio = 1 - (distanceTravelled / halfTravelDistance);
+		//	float rise = _projectileSpeed * distanceRatio * Time.deltaTime;
+		//	transform.position += new Vector3(0, rise, 0);
+
+		//	yield return null;
+		//}
+		//distanceTravelled = 0f;
+		//while (distanceTravelled < halfTravelDistance || !_hitThing)
+		//{
+		//	transform.position += (transform.forward * _projectileSpeed * Time.deltaTime);
+		//	distanceTravelled += Vector3.Distance(currentPos, transform.position);
+		//	currentPos = transform.position;
+
+		//	distanceRatio = distanceTravelled / halfTravelDistance;
+		//	float fall = _projectileSpeed * distanceRatio * Time.deltaTime;
+		//	transform.position += new Vector3(0, -fall, 0);
+
+		//	yield return null;
+		//}
+		//yield return null;
 	}
 }
 
